cc15449b621503eed75252af031d6137ac197c82
lahieuphong <hieuphong144@gmail.com>
2026-06-26 17:08:12 +0700

    feat(admin): customize admin branding

--- trungtamtrienlam-BE/config/settings/base.py
+++ trungtamtrienlam-BE/config/settings/base.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -96,6 +96,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
